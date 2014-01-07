dcee0c677ecaecc4d28ac4cb666cfb4579858f05
alessandro <a.degano@gmail.com>
2014-01-07 14:50:26 +0100

    Added configurations for chi_c and fixed couple of potential leaks in code.

---
+++ Ponia/CHARM_testAnalyzer_cfg.py
@@ -0,0 +1,62 @@
+debug_filename = 'debug.txt'
+
+import FWCore.ParameterSet.Config as cms
+process = cms.Process('test')
+process.load('Configuration.StandardSequences.MagneticField_AutoFromDBCurrent_cff')
+process.load('Configuration.Geometry.GeometryIdeal_cff')
+process.load("Configuration.StandardSequences.Reconstruction_cff")
+process.load('MuonAnalysis.MuonAssociators.patMuonsWithTrigger_cff')
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+process.GlobalTag.globaltag = cms.string('GR_E_V31::All')
+
+process.options   = cms.untracked.PSet( wantSummary = cms.untracked.bool(True) )
+process.MessageLogger.cerr.FwkReport.reportEvery = 500
+process.MessageLogger.destinations = cms.untracked.vstring(debug_filename)
+process.MessageLogger.cout = process.MessageLogger.cerr
+process.MessageLogger.cout.threshold = cms.untracked.string("DEBUG")
+
+input_filename = "file:9A42402F-DF68-E211-9E73-E41F131812CC.root"
+
+process.source = cms.Source(
+    "PoolSource",
+    fileNames = cms.untracked.vstring(input_filename)
+    )
+
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(-1)
+    )
+
+process.triggerSelection = cms.EDFilter("TriggerResultsFilter",
+                                        triggerConditions = cms.vstring('HLT_Dimuon8_Jpsi_v*'),
+                                        hltResults = cms.InputTag( "TriggerResults", "", "HLT" ),
+                                        l1tResults = cms.InputTag( "gtDigis" ),
+                                        l1tIgnoreMask = cms.bool( False ),
+                                        l1techIgnorePrescales = cms.bool( False ),
+                                        daqPartitions = cms.uint32( 1 ),
+                                        throw = cms.bool( True )
+                                        )
+
+process.load('Ponia.Modules.CHARM_chiCandProducer_cff')
+
+process.out = cms.OutputModule(
+    "PoolOutputModule",
+    fileName = cms.untracked.string('sel_chic.root'),
+    outputCommands =  cms.untracked.vstring('drop *',
+					    'keep *_offlinePrimaryVertices_*_*',
+#					    Temporary, testing if it solves excessive use of ram in chic analysis
+#                                            'keep *_dimuonProducer_UpsilonCandLorentzVector_*',
+                                            'keep *_chiCandProducer_chicand_*',
+					    'keep *_chiCandProducer_piZeroRejectCand_*',
+					    'keep *_refit_*_*'
+                                            ),
+    )
+
+process.end = cms.EndPath(process.out)
+
+#process.p = cms.Path(process.triggerSelection*process.chiSequence)
+process.p = cms.Path(process.triggerSelection*process.chiSequence)
+process.schedule = cms.Schedule(process.p,process.end)
+
+
+# HACK ! Need to understand !
+process.dimuonProducer.addMuonlessPrimaryVertex = False
---
+++ Ponia/Modules/python/CHARM_chiCandProducer_cff.py
@@ -0,0 +1,108 @@
+import FWCore.ParameterSet.Config as cms
+####################################################################################
+############################# CONFIGURATION PARAMETERS #############################
+####################################################################################
+
+tag_dimuon = 'dimuonProducer' # Tag name of the dimuon collection as saved from process.dimuonProducer
+cut_dimuon_Mass_low = 3.0
+cut_dimuon_Mass_high = 3.2
+cut_dimuon_Pt_min = 0.0
+cut_dimuon_rapidity = 1.6
+cut_dimuon_vprob = 0.01 # Minimum vertex probability for dimuon candidate
+tag_conversion = 'allConversions'
+conv_algo = 'undefined'
+conv_qual = ['highPurity','generalTracksOnly']
+tag_primary_vertex = 'offlinePrimaryVertices'
+conv_vertex_rho = 1.5
+conv_vtx_comp = False
+conv_tk_vtx = 5
+conv_inn_hits = True
+conv_min_dof = 3
+tag_chi_conv_prod = 'conversionProducer'
+tag_chi_conv_lab = 'conversions'
+tag_pfCandidates = 'particleFlow'
+pi0_online_switch = False
+pi0_small_min = 0.130
+pi0_small_max = 0.140
+pi0_large_min = 0.110
+pi0_large_max = 0.160
+chi_deltaM_min = 0.0    # This two values define the minimum and the maximum values 
+chi_deltaM_max = 2.0    # required for the QValue of the Chi candidate
+chi_dzMax = 0.5
+#upsilon_masses = [9.4603, 10.02326, 10.3552]
+jpsi_mass = 3.0969
+
+############################# CONFIGURATION END ####################################
+
+
+from MuonAnalysis.MuonAssociators.patMuonsWithTrigger_cff import *
+
+
+from Ponia.Configuration.MuonSelection import muonSelection2012
+selMuons = muonSelection2012.clone()
+selMuons.src='patMuonsWithTrigger'
+
+dimuonProducer = cms.EDProducer(
+	'DiMuonCandProducer',
+	muons=                    cms.InputTag('selMuons'),
+	beamSpotTag =             cms.InputTag('offlineBeamSpot'),
+	primaryVertexTag =        cms.InputTag('offlinePrimaryVertices'),
+	addCommonVertex=          cms.bool(True),
+	addMuonlessPrimaryVertex= cms.bool(True),
+        dimuonSelection = cms.string( 'p4.M > {0} && p4.M < {1} && p4.Pt > {2} && abs(y) < {3} && userFloat("vProb") > {4}'.format(cut_dimuon_Mass_low, cut_dimuon_Mass_high, cut_dimuon_Pt_min, cut_dimuon_rapidity, cut_dimuon_vprob) ),
+	theTriggerNames = cms.vstring('HLT_Dimuon8_Jpsi_v*'),
+        HLTLastFilters = cms.vstring('hltVertexmumuFilterDimuon8Jpsi') #HLT_Dimuon7_Upsilon_v{3,4,5,6,7}
+	)
+
+diMuonCount = cms.EDFilter(
+    'CandViewCountFilter',
+    src = cms.InputTag('dimuonProducer'),
+    minNumber = cms.uint32(1),
+    filter = cms.bool(True)
+    )
+
+conversionProducer = cms.EDProducer(
+    'PhotonConversionCandProducer',
+    conversions = cms.InputTag(tag_conversion),
+    convAlgo    = cms.string(conv_algo),
+    convQuality = cms.vstring(conv_qual),
+    primaryVertexTag = cms.InputTag(tag_primary_vertex),
+    convSelection = cms.string('conversionVertex.position.rho>{0}'.format(conv_vertex_rho) ),
+    wantTkVtxCompatibility = cms.bool(conv_vtx_comp),
+    sigmaTkVtxComp = cms.uint32(conv_tk_vtx),
+    wantCompatibleInnerHits = cms.bool(conv_inn_hits),
+    TkMinNumOfDOF = cms.uint32(conv_min_dof),
+    wantHighpurity = cms.bool(False),
+    vertexChi2ProbCut = cms.double(0.0005),
+    trackchi2Cut = cms.double(10),
+    minDistanceOfApproachMinCut = cms.double(-0.25),
+    minDistanceOfApproachMaxCut = cms.double(1.00)
+    )
+
+chiCandProducer = cms.EDProducer(
+    'ChiCandProducer',
+    conversions = cms.InputTag(tag_chi_conv_prod, tag_chi_conv_lab),
+    dimuons     = cms.InputTag('dimuonProducer'),
+    pfcandidates = cms.InputTag(tag_pfCandidates),
+    pi0OnlineSwitch = cms.bool(pi0_online_switch),
+    pi0SmallWindow   = cms.vdouble(pi0_small_min, pi0_small_max),
+    pi0LargeWindow   = cms.vdouble(pi0_large_min, pi0_large_max),
+    deltaMass   = cms.vdouble(chi_deltaM_min, chi_deltaM_max),
+    dzmax       = cms.double(chi_dzMax)
+    )
+
+refit = cms.EDProducer('ChibKinematicRefit',
+				 chi_cand = cms.InputTag("chiCandProducer","chicand"),
+				 conversions = cms.InputTag("chiCandProducer","chiConversions"),
+				 upsilon_mass = cms.double(jpsi_mass), # GeV  
+				 product_name = cms.string("chiCandRefit")
+				 )
+
+chiSequence = cms.Sequence(patMuonsWithTriggerSequence* 
+                            selMuons*   #official muon selection
+                            dimuonProducer*     #dimuon producer
+                            diMuonCount*
+                            conversionProducer*
+                            chiCandProducer*
+                            refit
+			)
